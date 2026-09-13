dff5fe5197478b1e02369b7f61384c2d6da43a6a
Dhrub Kumar Garg <dhrubkumargarg@gmail.com>
2026-09-13 09:35:52 +0530

    fix: refactored journey layout to use internal padding instead of GSAP offset calculations for bulletproof pinning

--- src/components/Manuscript.jsx
+++ src/components/Manuscript.jsx
@@ -183,7 +183,7 @@ function Timeline({ data }) {
     const tl = gsap.timeline({
       scrollTrigger: {
         trigger: section,
-        start: () => `top top+=${document.querySelector('.nav')?.offsetHeight || 80}`,
+        start: 'top top',
         end: '+=2500vh',
         pin: true,
         scrub: 0.6,
